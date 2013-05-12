e6207a079de387fe8db4eee65af4aeff4db025b8
noclat <noclat@gmail.com>
2013-05-12 19:06:28 +0200

    update render

--- site/core/render.php
+++ site/core/render.php
@@ -1,6 +1,6 @@
 <?php
 # Include a file with inner scope data
-  render($file, $data = array()) {
+  function render($file, $data = array()) {
     extract((array)$data);
     include $file.'.php';
   }
