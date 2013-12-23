f66d59dfb9a74d89148339abb7d8025100958db4
Nicolas Torres <noclat@gmail.com>
2013-12-23 15:34:18 +0100

    Update r76.php
    verb is now lowercase

--- r76.php
+++ r76.php
@@ -63,7 +63,7 @@
   function root() { return R76::root(); }
   function url($uri = false, $params = array()) { return R76::url($uri, $params); }
   function uri($k = false) { return R76::uri($k); }
-  function verb() { return $_SERVER['REQUEST_METHOD']; }
+  function verb() { return strtolower($_SERVER['REQUEST_METHOD']); }
   function async() { return strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest'; }
   function load($path) { foreach (glob(trim($path, '/').'/*.php') as $file) include_once $file; }
   function go($location = false) { if (!$location) $location = url(); header('location:'.$location); exit; }
